d3c48f612d23701d2b16e86f4421978789ee3c8c
Ryan Cromwell <ryan@echelontouch.com>
2011-11-06 22:24:58 -0500

    up through M

--- RomanNumeralsKata/Tests.cs
+++ RomanNumeralsKata/Tests.cs
@@ -23,6 +23,26 @@ namespace RomanNumeralsKata
         [TestCase(14, "XIV")]
         [TestCase(20, "XX")]
         [TestCase(29, "XXIX")]
+        [TestCase(40, "XL")]
+        [TestCase(41, "XLI")]
+        [TestCase(44, "XLIV")]
+        [TestCase(50, "L")]
+        [TestCase(51, "LI")]
+        [TestCase(90, "XC")]
+        [TestCase(96, "XCVI")]
+        [TestCase(100, "C")]
+        [TestCase(300, "CCC")]
+        [TestCase(400, "CD")]
+        [TestCase(450, "LD")]
+        [TestCase(500, "D")]
+        [TestCase(900, "CM")]
+        [TestCase(1000, "M")]
+        [TestCase(1987, "MCMLXXXVII")]
+        [TestCase(2000, "MM")]
+        [TestCase(2400, "MMCD")]
+        [TestCase(2600, "MMDC")]
+        [TestCase(3999, "MMMCMXCIX")]
+        [TestCase(3000, "MMM")]
         public void ConvertsSingleDigits(int number, string romanNumeral)
         {
             ConvertToRoman(number).Should().Equal(romanNumeral);
@@ -33,6 +53,16 @@ namespace RomanNumeralsKata
             var result = "";
             var map = new Dictionary<int, string>
                           {
+                              {3000, "MMM"},
+                              {1000, "M"},
+                              {900, "CM"},
+                              {500, "D"},
+                              {450, "LD"},
+                              {400, "CD"},
+                              {100, "C"},
+                              {90, "XC"},
+                              {50, "L"},
+                              {40, "XL"},
                               {10, "X"},
                               {9, "IX"},
                               {5, "V"},
